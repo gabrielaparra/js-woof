d0d35e2072d706996b392a9754507492a33f0c00
Gabriela Parra <parragabriela@icloud.com>
2017-07-03 09:11:43 -0400

    modified user model and sign up view

--- models/user-model.js
+++ models/user-model.js
@@ -11,10 +11,6 @@ const userSchema = new Schema({
     //minlength & maxlength for strings ONLY
     maxlength: 25
   },
-  username: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
     required: true,
--- routes/authorization-routes.js
+++ routes/authorization-routes.js
@@ -10,54 +10,55 @@ router.get('/signup', (req, res, next) => {
   res.render('authorization-views/signup-view.ejs');
 });
 
-// //STEP 2 of signup: processing the information submitted in the signup form
-// router.post('/signup', (req, res, next) => {
-//   if (req.body.signupUsername === '' || req.body.signupPassword === '') {
-//     res.locals.messageForDumbUsers = 'Please provide both username and password.';
-//     res.render('authorization-views/signup-view.ejs');
-//     return;
-//   }
-//
-//   //Checking if the username already exists in the database
-//   UserModel.findOne(
-//     { username: req.body.signupUsername },
-//     (err, userFromDb) => {
-//       //if there's an error retrieving the information from the DB
-//       if (err){
-//         next(err);
-//         return;
-//       }
-//
-//       //if a match was found (the username is taken)
-//       if (userFromDb) {
-//         res.locals.messageForDumbUsers = 'Sorry, that username is taken.';
-//         //display the form again with the feedback message
-//         res.render('authorization-views/signup-view.ejs');
-//         return;
-//       }
-//
-//       //if the username is available run these lines of code:
-//       const salt = bcrypt.genSaltSync(10);
-//       const scrambledPassword = bcrypt.hashSync(req.body.signupPassword, salt);
-//       //encrypting the user's input PW before saving it
-//
-//       const theUser = new UserModel({
-//         fullName: req.body.signupFullName,
-//         username: req.body.signupUsername,
-//         encryptedPassword: scrambledPassword
-//       });
-//
-//       theUser.save((err) => {
-//         if (err) {
-//           next(err);
-//           return;
-//         }
-//         res.redirect('/');
-//         //redirect to homepage
-//       });
-//     }
-//   );
-// });//--------------END REGISTRATION---------------
+//STEP 2 of signup: processing the information submitted in the signup form
+router.post('/signup', (req, res, next) => {
+  if (req.body.signupEmail === '' || req.body.signupPassword === '' || req.body.signupFullName === '') {
+    res.locals.messageForDumbUsers = 'Please provide all fields.';
+    res.render('authorization-views/signup-view.ejs');
+    return;
+  }
+
+  //Checking if the email already exists in the database
+  UserModel.findOne(
+    { email: req.body.signupEmail },
+    (err, userFromDb) => {
+      //if there's an error retrieving the information from the DB
+      if (err){
+        next(err);
+        return;
+      }
+
+      //if a match was found (the email is taken)
+      if (userFromDb) {
+        res.locals.messageForDumbUsers = 'Sorry, that email account is already registered.';
+        //display the form again with the feedback message
+        res.render('authorization-views/signup-view.ejs');
+        return;
+      }
+
+      //if the email is available run these lines of code:
+      const salt = bcrypt.genSaltSync(10);
+      const scrambledPassword = bcrypt.hashSync(req.body.signupPassword, salt);
+      //encrypting the user's input PW before saving it
+
+      const theUser = new UserModel({
+        fullName: req.body.signupFullName,
+        email: req.body.signupEmail,
+        encryptedPassword: scrambledPassword
+      });
+
+      theUser.save((err) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        res.redirect('/');
+        //redirect to homepage
+      });
+    }
+  );
+});
+//-----------------END OF REGISTRATION------------------
 //
 //
 // //-------------------LOG IN------------------------
